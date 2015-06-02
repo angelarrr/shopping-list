d6565ef12ed5df62e11183d625dcee11faf596c8
angelarrr <kwon.angela@gmail.com>
2015-06-02 15:48:33 -0400

    Shorten jQuery code

--- js/app.js
+++ js/app.js
@@ -1,16 +1,7 @@
 $(document).ready(function() {
-	/* Add item by pressing enter */
-	$("#add-items").keydown(function(enter) {
-		if(enter.keyCode == 13) {
-			var name = $('#add-items').val();
-			var added = '<li class="to-buy">' + '<div class="checkbox"><i class="fa fa-square-o"></i></div>' + '<div class="name">' + name + '</div>' + '<div class="remove"><i class="fa fa-times"></i></div></li>';
-			$('.list-items').prepend(added);
-			$('#add-items').val("");
-		}
-	});
-
-	/* Add item by pressing add button */
-	$(".add-button").on("click", function() {
+	/* Add item */
+	$("#items-to-add").submit(function(e) {
+		e.preventDefault();
 		var name = $('#add-items').val();
 		var added = '<li class="to-buy">' + '<div class="checkbox"><i class="fa fa-square-o"></i></div>' + '<div class="name">' + name + '</div>' + '<div class="remove"><i class="fa fa-times"></i></div></li>';
 		$('.list-items').prepend(added);
@@ -25,17 +16,11 @@ $(document).on("click", ".reset-button", function() {
 
 /* Delete items */
 $(document).on("click", ".remove", function() {
-	$(this).closest('li').fadeOut(500);
+	$(this).closest('li').fadeOut(500).remove();
 });
 
 /* check off items */
-$(document).on("click", ".checkbox", function(){
-    if ( $(this).closest('li').hasClass("to-buy") ) {
-        $(this).closest('li').removeClass("to-buy").addClass("bought");
-        $(this).find($(".fa")).removeClass("fa fa-square-o").addClass("fa fa-check");
-   }
-   else {
-        $(this).closest('li').removeClass("bought").addClass("to-buy");
-        $(this).find($(".fa")).removeClass("fa fa-check").addClass("fa fa-square-o");
-   }
-}); 
+$(document).on("click", ".checkbox", function() {
+	$(this).closest('li').toggleClass("to-buy bought");
+	$(this).children('.fa').toggleClass("fa-square-o fa-check");
+})
